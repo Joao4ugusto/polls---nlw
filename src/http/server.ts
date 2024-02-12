09300aa281f6fe41134f7e3e94451183ccdada07
João Augusto <joaoaugusto311016@gmail.com>
2024-02-11 23:13:33 -0300

    third commit

--- src/http/server.ts
+++ src/http/server.ts
@@ -3,6 +3,8 @@ import cookie from "@fastify/cookie";
 import { createPoll } from "./routes/create-poll";
 import { getPoll } from "./routes/get-poll";
 import { voteOnPoll } from "./routes/vote-on-poll";
+import fastifyWebsocket from "@fastify/websocket";
+import { pollResults } from "./aws/poll-results";
 
 const app = fastify();
 
@@ -15,6 +17,9 @@ app.register(cookie, {
 app.register(createPoll);
 app.register(getPoll);
 app.register(voteOnPoll);
+app.register(fastifyWebsocket);
+
+app.register(pollResults)
 
 app.listen({port: 3333}).then(() => {
   console.log('HTTP server running');
